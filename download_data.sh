4041b986f3dc7f6deb0ee6bab85b3c723c34bb32
Caelan Osman <caelano@byu.edu>
2020-09-03 12:58:10 -0600

    unix shell 1 lab

--- download_data.sh
+++ download_data.sh
@@ -2,9 +2,7 @@
 # Download data files and place them in the lab folders.
 
 SOURCE="https://github.com/Foundations-of-Applied-Mathematics/Data.git"
-PYTHONESSENTIALS=("Exceptions_FileIO" "Profiling")
-DATASCIENCEESSENTIALS=("UnixShell1" "SQL1" "SQL2")
-VOLUME1=("Conditioning_Stability" "Differentiation" "DrazinInverse" "FacialRecognition" "ImageSegmentation" "LeastSquares_Eigenvalues" "LinearTransformations" "PageRank" "SVD_ImageCompression")
+LABS=("Conditioning_Stability" "Differentiation" "DrazinInverse" "FacialRecognition" "ImageSegmentation" "LeastSquares_Eigenvalues" "LinearTransformations" "PageRank" "SVD_ImageCompression")
 GIT="https://git-scm.com"
 TEMPDIR="_DATA_"`date +%s`"_"
 
@@ -18,13 +16,7 @@ cd ${TEMPDIR}
 git init --quiet
 git remote add origin "${SOURCE}"
 git config core.sparseCheckout true
-for lab in ${PYTHONESSENTIALS[@]}; do
-    echo "${lab}" >> .git/info/sparse-checkout
-done
-for lab in ${DATASCIENCEESSENTIALS[@]}; do
-    echo "${lab}" >> .git/info/sparse-checkout
-done
-for lab in ${VOLUME1[@]}; do
+for lab in ${LABS[@]}; do
     echo "${lab}" >> .git/info/sparse-checkout
 done
 echo -e "\nInitializing Download ...\n"
@@ -34,26 +26,10 @@ cd ../
 # Migrate the files from the temporary folder.
 set +e
 echo -e "\nMigrating files ..."
-for lab in ${PYTHONESSENTIALS[@]}; do
+for lab in ${LABS[@]}; do
     # Check that the target directory exists before copying.
     if [ -d "./${lab}" ]; then
-        cp -v ${TEMPDIR}/PythonEssentials/${lab}/* ./${lab}/
-    else
-        echo -e "\nERROR: directory '${lab}' not found!\n"
-    fi
-done
-for lab in ${DATASCIENCEESSENTIALS[@]}; do
-    # Check that the target directory exists before copying.
-    if [ -d "./${lab}" ]; then
-        cp -rv ${TEMPDIR}/DataScienceEssentials/${lab}/* ./${lab}/
-    else
-        echo -e "\nERROR: directory '${lab}' not found!\n"
-    fi
-done
-for lab in ${VOLUME1[@]}; do
-    # Check that the target directory exists before copying.
-    if [ -d "./${lab}" ]; then
-        cp -v ${TEMPDIR}/Volume1/${lab}/* ./${lab}/
+        cp -v ${TEMPDIR}/${lab}/* ./${lab}/
     else
         echo -e "\nERROR: directory '${lab}' not found!\n"
     fi
