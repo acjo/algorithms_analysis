a4cb4f94148bdf282625738e7e6f752dbd1c6213
Caelan Osman <caelano@byu.edu>
2020-09-14 18:41:28 -0600

    adding comments to shut the box game

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -16,7 +16,7 @@ from random import randint
 
 # Problem 1
 def prob1(L):
-    """Return the minimum, maximum, and average of the entries of L
+    """Returns the minimum, maximum, and average of the entries of L
     (in that order).
     """
     return min(L), max(L), sum(L) / len( L)
@@ -67,80 +67,66 @@ def prob2():
 """Imports the calculator module"""
 def hypot(a, b):
     """Calculate and return the length of the hypotenuse of a right triangle.
-    Do not use any functions other than those that are imported from your
-    'calculator' module.
-
-    Parameters:
-        a: the length one of the sides of the triangle.
-        b: the length the other non-hypotenuse side of the triangle.
-    Returns:
-        The length of the triangle's hypotenuse.
+    all functions imported from the calculator.py module
     """
     return c.sqrt(c.addition(c.product(a,a), c.product(b,b)))
 
 # Problem 4
 def power_set(A):
-    """Use itertools to compute the power set of A.
-
-    Parameters:
-        A (iterable): a str, list, set, tuple, or other iterable collection.
-
-    Returns:
-        (list(sets)): The power set of A as a list of sets.
-    """
+    '''This function outputs a list that mimimics the power set of A'''
     #size of powerset |p(A)|= 2^n where n is the cardinality of A.
     power_list = []
     for i in range(len(A)):
-        if i == 0:
+        if i == 0: #adding our empty set
             power_list.append(set())
-        else:
+        else: #getting all subsets of A given of size i
             sub_list = list((combinations(A,i)))
             #I'm adding this nested for loop so that each subset of A will be it's own set
             # as opposed to the entire combination list being it's own set
-            for j in range(len(sub_list)):
-                power_list.append(set(sub_list[j]))
-    power_list.append(A)
-    return power_list
+            for j in range(len(sub_list)): #this iterates over the sub list and makes each
+                power_list.append(set(sub_list[j])) # element a set
+    power_list.append(A)# adding the set A to our power set
+    return power_list #return the power set
 
 if __name__ == "__main__":
-    #making the program such that this does not run when/if the file is imported
+    #making the program such that this function does not get imported if this file is used as a module.
     # Problem 5: Implement shut the box.
     def shut_the_box(player, timelimit):
         """Play a single game of shut the box."""
-        remaining_numbers = list(range(1,10))
-        length = len(remaining_numbers)
-        start_time = time.time()
-        end_time = 0
-        while len(remaining_numbers) > 0 and end_time - start_time <= int(timelimit):
-            print("Numbers left: " + str(remaining_numbers))
-            if sum(remaining_numbers) <= 6:
+        remaining_numbers = list(range(1,10)) #initialize our set of numbers to remove 1 through 9.
+        length = len(remaining_numbers) #gets the length of the remaining nubers list
+        start_time = time.time() #starts our timer
+        end_time = 0 #initalizes the end time to zero for the condition of the while loop
+        while len(remaining_numbers) > 0 and end_time - start_time <= int(timelimit): #executes while there are remaining numbers or while the time has not run out. 
+            print("Numbers left: " + str(remaining_numbers)) #prints the remaining numbers
+            if sum(remaining_numbers) <= 6: #if the sum of the remaining numbers is less than 6 then the roll has to be between 1 and 6. 
                 roll = randint(1,6)
-                print("Roll: " + str(roll))
-            else:
+                print("Roll: " + str(roll)) #print the roll
+            else:#otherwise the roll is between 1 and 12
                 roll = randint(1, 12)
-                print("Roll: " + str(roll))
-            if not box.isvalid(roll, remaining_numbers):
+                print("Roll: " + str(roll)) #print the role
+            if not box.isvalid(roll, remaining_numbers): #check if the roll is valid, there are numbers left that can sum to the roll
                 print("Game over! \n")
-                end_time = time.time()
-                break
-            if len(remaining_numbers) == length:
+                end_time = time.time() #if this isn't the case game is over, set end time variable
+                break #exit the while loop/game
+            if len(remaining_numbers) == length: #checks if it is the first iteration and just prints the time limit
                 print("Seconds left: " + str(timelimit))
-            else:
+            else: #if it isn't the first iteration printing the time limit minus the time played
                 print("Seconds left: " + str(round(int(timelimit) - end_time + start_time, 2)))
-            player_numbers = []
-            while len(player_numbers) == 0:
-                player_choice = input("Numbers to eliminate: ")
-                player_numbers = box.parse_input(player_choice, remaining_numbers)
-                if sum(player_numbers) != roll:
-                    print("Invalid inputs \n")
+            player_numbers = [] # intialize a empty lis to hold the player number
+            while len(player_numbers) == 0: #enters this while loop if the player numbers length is 0
+                player_choice = input("Numbers to eliminate: ") #asks the input number
+                player_numbers = box.parse_input(player_choice, remaining_numbers) #parses the input
+                if sum(player_numbers) != roll: #makes sure the choice actually sums to the roll
+                    print("Invalid inputs \n") #if it doesn't make them choose new numbers and output the time they have left. 
                     end_time = time.time()
-                    print("Seconds left: " + str(round(int(timelimit) - end_time + start_time, 2)))
-                    player_numbers = []
-            for num in player_numbers:
+                    print("Seconds left: " + str(round(int(timelimit) - end_time + start_time, 2))) #print the time
+                    player_numbers = [] #empty the player_numbers list
+            for num in player_numbers: #if the numbers are valid remove them from the list
                 remaining_numbers.remove(num)
-            end_time = time.time()
+            end_time = time.time() #set end time variable
             print("\n")
-
+        #outputs the score depending on if you won or lost.
         print("Score for player " + str(player) + ": " + str(sum(remaining_numbers)))
         print("Time played: " + str(end_time - start_time))
         if sum(remaining_numbers) == 0 and end_time - start_time <= timelimit:
@@ -148,7 +134,7 @@ if __name__ == "__main__":
         else:
             print("Better luck next time! >:)")
         return
-
+#this checks to make sure that the shut the box function can only be called with 3 arguments. otherwise, it won't play the game
     if len(sys.argv) == 3:
         shut_the_box(sys.argv[1], sys.argv[2])
     elif len(sys.argv) > 3:
