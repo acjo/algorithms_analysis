9de18f0ddffbe4952fda3498e2c540cb8298d3ae
Caelan Osman <caelano@byu.edu>
2020-10-24 15:10:06 -0600

    prob 1-2

--- QR_Decomposition/qr_decomposition.py
+++ QR_Decomposition/qr_decomposition.py
@@ -1,51 +1,42 @@
 # qr_decomposition.py
 """Volume 1: The QR Decomposition.
-<Name>
-<Class>
-<Date>
+Caelan Osman
+Math 345 Sec 3
+October 23, 2020
 """
 
+import numpy as np
+from scipy import linalg as la
 
 # Problem 1
 def qr_gram_schmidt(A):
-    """Compute the reduced QR decomposition of A via Modified Gram-Schmidt.
-
-    Parameters:
-        A ((m,n) ndarray): A matrix of rank n.
-
-    Returns:
-        Q ((m,n) ndarray): An orthonormal matrix.
-        R ((n,n) ndarray): An upper triangular matrix.
+    """ This function computes the reduced QR decomposition of A via Modified Gram-Schmidt.
+        Note: The matrix A (m x n) has to have rank n which is less than or equaal to m.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
-
+    n = A.shape[1] #get the size that will help form Q and R
+    Q = np.copy(A).astype(np.float64) #copy A and change type to float
+    R = np.zeros((n,n)) #intialize R with an nxn matrix of zeros
+    for i in range(0, n):
+        R[i, i] = la.norm(Q[:, i]) #diagonals will be set to the norm of the ith column
+        Q[:, i] /= R[i, i] #normalize the columns of Q
+        for j in range (i+1, n):
+            R[i, j] = (Q[:, j]).T @ (Q[:, i]) #make the upper triangluar elements of R
+            Q[:, j] -= (R[i, j] * Q[:, i]) #orthognoalize the jth column of Q
+    return Q, R
 
 # Problem 2
 def abs_det(A):
     """Use the QR decomposition to efficiently compute the absolute value of
-    the determinant of A.
-
-    Parameters:
-        A ((n,n) ndarray): A square matrix.
-
-    Returns:
-        (float) the absolute value of the determinant of A.
+    the determinant of A. A needs to be invertible
     """
-    raise NotImplementedError("Problem 2 Incomplete")
-
+    return abs(np.prod(np.diag(la.qr(A)[1])))
 
 # Problem 3
 def solve(A, b):
     """Use the QR decomposition to efficiently solve the system Ax = b.
-
-    Parameters:
-        A ((n,n) ndarray): An invertible matrix.
-        b ((n, ) ndarray): A vector of length n.
-
-    Returns:
-        x ((n, ) ndarray): The solution to the system Ax = b.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+
+
 
 
 # Problem 4
