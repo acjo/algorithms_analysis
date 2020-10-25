606f2af44d3fba49ac5a4f5da2077084fae4da0a
Caelan Osman <caelano@byu.edu>
2020-10-25 00:01:12 -0600

    finished

--- QR_Decomposition/qr_decomposition.py
+++ QR_Decomposition/qr_decomposition.py
@@ -46,42 +46,43 @@ def solve(A, b):
         for j in range(i + 1, rows_x): #subtracting off previous x elements
             x[i] -= R[i, j] * x[j]
         x[i] /= R[i, i] #isolating x[i]
+
     return x
-
-A = np.array([[1,2,3], [4,5,6], [7, 8, 10]])
-b = np.array([12, 2, 5])
-
-print(solve(A, b))
-print()
-print(la.solve(A, b))
-
-
-
 
 # Problem 4
 def qr_householder(A):
     """Compute the full QR decomposition of A via Householder reflections.
-
-    Parameters:
-        A ((m,n) ndarray): A matrix of rank n.
-
-    Returns:
-        Q ((m,m) ndarray): An orthonormal matrix.
-        R ((m,n) ndarray): An upper triangular matrix.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    sign = lambda x: 1 if x >= 0 else -1 #used for setting u
 
+    m, n = A.shape #get shape of A
+    R = np.copy(A).astype(np.float64) #copy and change t ype
+    Q = np.eye(m) #create a m x m identity matrix
+    for k in range(0, n):
+        u = np.copy(R[k:, k]) #initialize u
+        u[0] += sign(u[0]) * la.norm(u) #reassign the first element
+        u /= la.norm(u) #normalize
+        R[k:, k:] -= 2 * np.outer(u, np.dot(u, R[k:, k:])) #relfect R
+        Q[k:, :] -= 2 * np.outer(u, (u.T @ Q[k:, :])) #reflect Q
+
+    return Q.T, R
 
 # Problem 5
 def hessenberg(A):
     """Compute the Hessenberg form H of A, along with the orthonormal matrix Q
-    such that A = QHQ^T.
-
-    Parameters:
-        A ((n,n) ndarray): An invertible matrix.
-
-    Returns:
-        H ((n,n) ndarray): The upper Hessenberg form of A.
-        Q ((n,n) ndarray): An orthonormal matrix.
+    such that A = QHQ^T. A has to be nonsingular
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    sign = lambda x: 1 if x >= 0 else -1 #used for setting u
+
+    m, n = A.shape #get shape of A
+    H = np.copy(A).astype(np.float64) #copy A and set type
+    Q = np.eye(m) #create a mxm identity amtrix
+    for k in range(0, n-2):
+        u = np.copy(H[k+1:, k]) #initialize u
+        u[0] += sign(u[0]) * la.norm(u) # reset the first element
+        u /= la.norm(u) #normalize u
+        H[k+1:, k:] -= 2 * np.outer(u, u.T @ H[k+1:, k:]) #apply Qk to H
+        H[:, k+1:] -= 2 * np.outer(H[:, k+1:] @ u, u.T) #apply QkT to H
+        Q[k+1:, :] -= 2 * np.outer(u, u.T @ Q[k+1:, :]) #apply Qk to Q
+
+    return H, Q.T
