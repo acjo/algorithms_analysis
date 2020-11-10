7608985b8aad3cb404182fac468cdfbf86c5a845
Caelan Osman <caelano@byu.edu>
2020-11-10 12:18:47 -0700

    finished problem

--- SVD_ImageCompression/svd_image_compression.py
+++ SVD_ImageCompression/svd_image_compression.py
@@ -8,6 +8,7 @@ Nov. 9, 2020
 import numpy as np
 from numpy import linalg as la
 from scipy import linalg as spla
+from matplotlib import pyplot as plt
 
 # Problem 1
 def compact_svd(A, tol=1e-6):
@@ -25,7 +26,7 @@ def compact_svd(A, tol=1e-6):
     #get eigenvalues/vectors
     eig_vals, eig_vecs = la.eig(A.conj().T @ A)
     #compute singular values
-    singular_vals = np.sqrt(eig_vals)
+    singular_vals = np.sqrt(abs(eig_vals))
     #get sort indexing in descending order
     ranking = np.argsort(singular_vals)[::-1]
 
@@ -47,7 +48,54 @@ def visualize_svd(A):
     """Plot the effect of the SVD of A as a sequence of linear transformations
     on the unit circle and the two standard basis vectors.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    #create x and y coordinates
+    theta = np.linspace(0, 2 * np.pi, 200)
+    x = np.cos(theta)
+    y = np.sin(theta)
+
+    #create S
+    S = np.vstack((x, y))
+    #create E
+    E = np.array([[1, 0, 0], [0, 0, 1]])
+    #compute SVD
+    U, sigma, Vh = spla.svd(A, full_matrices=True)
+
+    #plot original
+    ax1 = plt.subplot(221)
+    ax1.plot(x, y)
+    ax1.plot(E[0, :], E[1, :])
+    ax1.set_title('Original')
+    plt.axis('equal')
+
+    #plot second operation
+    operation_1_S = Vh @ S
+    operation_1_E = Vh @ E
+    ax2 = plt.subplot(222)
+    ax2.plot(operation_1_S[0, :], operation_1_S[1, :])
+    ax2.plot(operation_1_E[0, :], operation_1_E[1, :])
+    ax2.set_title('1st Operation')
+    plt.axis('equal')
+
+    #plot 3rd operation
+    operation_2_S = np.diag(sigma) @ operation_1_S
+    operation_2_E = np.diag(sigma) @ operation_1_E
+    ax3 = plt.subplot(223)
+    ax3.plot(operation_2_S[0, :], operation_2_S[1, :])
+    ax3.plot(operation_2_E[0, :], operation_2_E[1, :])
+    ax3.set_title('2nd Operation')
+    plt.axis('equal')
+
+    #plot final operation
+    operation_3_S = U @ operation_2_S
+    operation_3_E = U @ operation_2_E
+    ax4 = plt.subplot(224)
+    ax4.plot(operation_3_S[0, :], operation_3_S[1, :])
+    ax4.plot(operation_3_E[0, :], operation_3_E[1, :])
+    ax4.set_title('3rd Operation')
+    plt.axis('equal')
+
+    plt.show()
+
 
 
 # Problem 3
