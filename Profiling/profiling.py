b1de60b84cc063de6e860c9e5f37652185ea2462
Caelan Osman <caelano@byu.edu>
2021-01-16 18:22:23 -0700

    fixing comments

--- Profiling/profiling.py
+++ Profiling/profiling.py
@@ -37,21 +37,29 @@ def max_path(filename="triangle.txt"):
 
 def max_path_fast(filename="triangle_large.txt"):
     """Find the maximum vertical path in a triangle of values."""
+    #get the data
     with open(filename, 'r') as infile:
         data = infile.read().strip().split('\n')
 
+    #iterate from bottom
     for i in range(len(data) - 2, -1, -1):
+        #if we are just starting set previous to the last row
         if i == len(data) - 2:
-            previous_l = data[i + 1].split(' ')
+            prev_line = data[i + 1].split(' ')
+        #otherwise set previous_line to current
         else:
-            previous_l = current_l
-        current_l = data[i].split(' ')
-        for j in range(len(current_l)):
-            s1 = int(current_l[j]) + int(previous_l[j])
-            s2 = int(current_l[j]) + int(previous_l[j + 1])
-            current_l[j] = max([s1, s2])
+            prev_line = curr_line
+        #set current line
+        curr_line = data[i].split(' ')
+        #iterate through the current line and get the max of the current number
+        #and the two below it
+        for j, number in enumerate(curr_line):
+            s1 = int(number) + int(prev_line[j])
+            s2 = int(number) + int(prev_line[j + 1])
+            curr_line[j] = max([s1, s2])
 
-    return current_l[0]
+    #return the first number as the max
+    return curr_line[0]
 
 # Problem 2
 def primes(N):
@@ -68,26 +76,38 @@ def primes(N):
         current += 1
     return primes_list
 
-
+#FIXME
 def primes_fast(N):
     """Compute the first N primes."""
+    # return an empty list
     if N == 0:
         return []
+    #initalize list with 2
     primes_list = [2]
+    #set current to 3
     current = 3
-    l = len(primes_list)
-    while l < N:
+    #initialize length
+    length = len(primes_list)
+    while length < N:
         isprime = True
         for prime in primes_list:
+            #the current prime is greater than the
+            #sqrt of the current it has to be prime
             if prime > int(np.sqrt(current)):
                 break
-            if current % prime == 0:
+            #if the current is divisble by prime then it
+            #can't be prime
+            elif current % prime == 0:
                 isprime = False
                 break
         if isprime:
+            #add prime to list and increment length
             primes_list.append(current)
-            l += 1
+            length += 1
+        #cont up by two
         current += 2
+
+    #return list of primes
     return primes_list
 
 
@@ -118,6 +138,8 @@ def nearest_column_fast(A, x):
     Returns:
         (int): The index of the column of A that is closest in norm to x.
     """
+    #return the index of the column with the smallest norm in A - x
+    #have to stack it so array broadcasting works correctl
     return np.argmin(np.linalg.norm(A - np.vstack(x), axis=0))
 
 # Problem 4
@@ -139,18 +161,26 @@ def name_scores(filename="names.txt"):
 
 def name_scores_fast(filename='names.txt'):
     """Find the total of the name scores in the given file."""
+    #import the names
     with open(filename, 'r') as infile:
         names = sorted(infile.read().replace('"', '').split(','))
 
+    #set our alphabet
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    #create a dictionary of key value pairs for each letter in alphabet
     alphabet_values = {letter: (i + 1) for i, letter in enumerate(alphabet)}
 
+    #total count initialize to zero
     total = 0
+    #iterate through all names
     for i, name in enumerate(names):
+        #initial name value to zero
         name_value = 0
         for letter in name:
+            #get the value for the letter key and add it to name value
             if letter in alphabet_values:
                 name_value += alphabet_values[letter]
+        #multiply name value by the score (index of name + 1) and add to total
         total += name_value * (i + 1)
 
     return total
@@ -159,19 +189,26 @@ def name_scores_fast(filename='names.txt'):
 # Problem 5
 def fibonacci():
     """Yield the terms of the Fibonacci sequence with F_1 = F_2 = 1."""
+    #booleans to help with the loop and what to return
     first = True
     second = True
     cont = True
     while cont:
+        #if it is not the first and not the second yield Fn = Fn_2 + Fn_1
+        #and set Fn_2 = Fn_1 and Fn_1 = Fn
         if not first and not second:
             Fn = Fn_1 + Fn_2
             yield Fn
             Fn_2 = Fn_1
             Fn_1 = Fn
+        #if it is first set Fn_2 = 1 and yield Fn_2
+        #set first to False
         if first:
             Fn_2 = 1
             yield Fn_2
             first = False
+        #if it is first set Fn_1 = 1 and yiled Fn_2
+        #set second to false
         if second:
             Fn_1 = 1
             yield Fn_1
@@ -184,27 +221,39 @@ def fibonacci_digits(N=1000):
     Returns:
         (int): The index.
     """
-    iterate = True
-    generator = fibonacci()
+    #get the generator
+    fib = fibonacci()
+    #initial index and digits to 0
     index = 0
-    while iterate:
+    digits = 0
+    while digits < N:
+        #increment index
         index += 1
-        current = next(generator)
-        rep = str(current)
-        if len(rep) == N:
-            return index
+        #call the generator to get current number
+        current = next(fib)
+        #set digits to the length of the string
+        #representation of current
+        digits = len(str(current))
 
+    #return the index
+    return index
 
 # Problem 6
 def prime_sieve(N):
     """Yield all primes that are less than N."""
+    # No primes are less than or equal to 1
     if N <= 1:
         return
+    #set the numbers sipping by 2 from 3
     numbers = np.array([num for num in range(3, N+1, 2)])
+    #yield 2
     yield 2
     while numbers.size > 0:
+        #find which elements of numbers is divisible by the first element
         mask = np.remainder(numbers, numbers[0]) == 0
+        #yield the first
         yield numbers[0]
+        #apply the negation of the mask to numbers
         numbers = numbers[~mask]
 
 
@@ -224,6 +273,7 @@ def matrix_power(A, n):
             product[i] = temporary_array
     return product
 
+#apply the @jit function decorator for numba enhancement
 @jit
 def matrix_power_numba(A, n):
     """Compute A^n, the n-th power of the matrix A, with Numba optimization."""
@@ -244,32 +294,39 @@ def prob7(n=10):
     """Time matrix_power(), matrix_power_numba(), and np.linalg.matrix_power()
     on square matrices of increasing size. Plot the times versus the size.
     """
+    #set A as a random small matrix and call matrix_power_numba() to compile function
     A = np.random.random((2, 2))
-    #get it to compile
     matrix_power_numba(A, 1)
 
+    #initialize empty lists for numba time, numpy time, and python time
     numba = []
     num_py = []
     python = []
+    #set our ms
     ms = [num for num in range(2, 8)]
     for m in ms:
+        #get A
         A = np.random.random((2**m, 2**m))
 
+        #time numba
         numba_start = time.time()
         matrix_power_numba(A, n)
         numba_end = time.time()
         numba.append(numba_end - numba_start)
 
+        #time numpy function
         numpy_start = time.time()
         la.matrix_power(A, n)
         numpy_end = time.time()
         num_py.append(numpy_end - numpy_start)
 
+        #time python function
         python_start = time.time()
         matrix_power(A, n)
         python_end = time.time()
         python.append(python_end - python_start)
 
+    #plot on a logplot the power (m) with respect to the times
     plt.loglog(ms, numba,'c-', label= 'Numba Optimized')
     plt.loglog(ms, num_py, 'r--', label='NumPy Optimized')
     plt.loglog(ms, python, 'm-.', label='Python Code')
