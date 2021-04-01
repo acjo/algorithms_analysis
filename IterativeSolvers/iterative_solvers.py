a62fabb47658fffba521454de782d66e992c05d9
Caelan Osman <caelano@byu.edu>
2021-04-01 12:46:06 -0600

    first 3 problems

--- IterativeSolvers/iterative_solvers.py
+++ IterativeSolvers/iterative_solvers.py
@@ -6,7 +6,8 @@ March 30, 2021
 """
 
 import numpy as np
-
+from scipy import linalg as la
+from matplotlib import pyplot as plt
 
 # Helper function
 def diag_dom(n, num_entries=None):
@@ -33,7 +34,7 @@ def diag_dom(n, num_entries=None):
     return A
 
 # Problems 1 and 2
-def jacobi(A, b, tol=1e-8, maxiter=100):
+def jacobi(A, b, tol=1e-8, maxiter=100, plot=False):
     """Calculate the solution to the system Ax = b via the Jacobi Method.
 
     Parameters:
@@ -45,8 +46,48 @@ def jacobi(A, b, tol=1e-8, maxiter=100):
     Returns:
         ((n,) ndarray): The solution to system Ax = b.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    #get diagonal, Lower and upper triangular matrices
+    D_inv = 1/np.diag(A)
 
+    x0 = np.ones(D_inv.size)
+    #TODO ask about initial point
+    #TODO ask about which convergence check to use
+
+    #if we want to plot aboslute error
+    if plot:
+        #error vector
+        error = []
+        error.append(la.norm(A@x0 - b, ord=np.inf))
+        for i in range(maxiter):
+            #create next iteration
+            x1 = x0 + D_inv * (b - A @ x0)
+            #append error
+            error.append(la.norm(A@x1 - b, ord=np.inf))
+            #check convergence
+            if la.norm(x0 - x1, ord=np.inf) < tol:
+                break
+            #update previous iteration
+            x0 = x1
+
+        #plot on log plot absolute convergence rates
+        plt.semilogy(np.arange(0, i+2), error, 'm-')
+        plt.title('Convergence of Jacobi Method')
+        plt.xlabel('Iteration Count')
+        plt.ylabel('Absolute Error of Approximation')
+        plt.show()
+
+    #if we don't want to plot aboslute error
+    else:
+        for i in range(maxiter):
+            #create next iteration
+            x1 = x0 + D_inv * (b - A @ x0)
+            #check convergence
+            if la.norm(x0 - x1, ord=np.inf) < tol:
+                break
+            #update previous iteration
+            x0 = x1
+
+    return x1
 
 # Problem 3
 def gauss_seidel(A, b, tol=1e-8, maxiter=100, plot=False):
@@ -62,7 +103,41 @@ def gauss_seidel(A, b, tol=1e-8, maxiter=100, plot=False):
     Returns:
         x ((n,) ndarray): The solution to system Ax = b.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    #initialize vectors
+    n = A.shape[1]
+    x0 = np.ones(n)
+
+    #if we want to plot
+    if plot:
+        #intialize error vector
+        error = []
+        error.append(la.norm(A@x0 - b, ord=np.inf))
+        for i in range(maxiter):
+            #update x0
+            x1 = np.array([x0[j] + (b[j] - np.inner(A[j, :], x0)) /A[j, j] for j in range(n)])
+            error.append(la.norm(A@x1 - b, ord=np.inf))
+            #check convergence
+            if la.norm(x0-x1, ord=np.inf) < tol:
+                break
+            x0 = x1
+
+        plt.semilogy(np.arange(0, i+2), error, 'm-')
+        plt.title('Convergence of Gauss Seidel Method')
+        plt.xlabel('Iteration Count')
+        plt.ylabel('Absolute Error of Approximation')
+        plt.show()
+
+    #if we don't want to plot
+    else:
+        for i in range(maxiter):
+            #update x0
+            x1 = np.array([x0[j] + (b[j] - np.inner(A[j, :], x0)) /A[j, j] for j in range(n)])
+            #check convergence
+            if la.norm(x0-x1, ord=np.inf) < tol:
+                break
+            x0 = x1
+
+    return x1
 
 
 # Problem 4
@@ -130,3 +205,29 @@ def prob7():
     computed as a function of omega.
     """
     raise NotImplementedError("Problem 7 Incomplete")
+
+
+
+if __name__ == "__main__":
+
+    #prob1 and 2
+    '''
+    b = np.random.random(100)
+    A = diag_dom(100)
+    x = jacobi(A, b)
+    print(np.allclose(A@x, b))
+    x1 = jacobi(A, b, plot=True)
+    print(np.allclose(A@x1, b))
+    print(np.allclose(x, x1))
+    '''
+
+    #prob3
+    '''
+    b = np.random.random(30)
+    A = diag_dom(30)
+    x2 = gauss_seidel(A, b, plot=False)
+    print(np.allclose(A@x2, b))
+    x3 = gauss_seidel(A, b, plot=True)
+    print(np.allclose(A@x3, b))
+    print(np.allclose(x2, x3))
+    '''
