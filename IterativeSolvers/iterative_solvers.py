466b74f1c827d014b43771d28814ada754e387c4
Caelan Osman <caelano@byu.edu>
2021-04-01 21:30:58 -0600

    need to fix prob 7

--- IterativeSolvers/iterative_solvers.py
+++ IterativeSolvers/iterative_solvers.py
@@ -6,8 +6,10 @@ March 30, 2021
 """
 
 import numpy as np
+from scipy import sparse
 from scipy import linalg as la
 from matplotlib import pyplot as plt
+from scipy.sparse import linalg as spla
 
 # Helper function
 def diag_dom(n, num_entries=None):
@@ -49,9 +51,8 @@ def jacobi(A, b, tol=1e-8, maxiter=100, plot=False):
     #get diagonal, Lower and upper triangular matrices
     D_inv = 1/np.diag(A)
 
-    x0 = np.ones(D_inv.size)
-    #TODO ask about initial point
-    #TODO ask about which convergence check to use
+    #x0 = np.ones(D_inv.size)
+    x0 = np.random.random(D_inv.size)
 
     #if we want to plot aboslute error
     if plot:
@@ -66,10 +67,10 @@ def jacobi(A, b, tol=1e-8, maxiter=100, plot=False):
             #check convergence
             if la.norm(x0 - x1, ord=np.inf) < tol:
                 break
-            #update previous iteration
+            #update previous
             x0 = x1
 
-        #plot on log plot absolute convergence rates
+        #plot absolute convergence rates on logplot
         plt.semilogy(np.arange(0, i+2), error, 'm-')
         plt.title('Convergence of Jacobi Method')
         plt.xlabel('Iteration Count')
@@ -84,7 +85,7 @@ def jacobi(A, b, tol=1e-8, maxiter=100, plot=False):
             #check convergence
             if la.norm(x0 - x1, ord=np.inf) < tol:
                 break
-            #update previous iteration
+            #update previous
             x0 = x1
 
     return x1
@@ -105,7 +106,8 @@ def gauss_seidel(A, b, tol=1e-8, maxiter=100, plot=False):
     """
     #initialize vectors
     n = A.shape[1]
-    x0 = np.ones(n)
+    #x0 = np.ones(n)
+    x0 = np.random.random(n)
 
     #if we want to plot
     if plot:
@@ -113,14 +115,16 @@ def gauss_seidel(A, b, tol=1e-8, maxiter=100, plot=False):
         error = []
         error.append(la.norm(A@x0 - b, ord=np.inf))
         for i in range(maxiter):
-            #update x0
+            #update x1
             x1 = np.array([x0[j] + (b[j] - np.inner(A[j, :], x0)) /A[j, j] for j in range(n)])
             error.append(la.norm(A@x1 - b, ord=np.inf))
             #check convergence
             if la.norm(x0-x1, ord=np.inf) < tol:
                 break
+            #update previous
             x0 = x1
 
+        #plot absolute convergence rates on logplot
         plt.semilogy(np.arange(0, i+2), error, 'm-')
         plt.title('Convergence of Gauss Seidel Method')
         plt.xlabel('Iteration Count')
@@ -130,12 +134,15 @@ def gauss_seidel(A, b, tol=1e-8, maxiter=100, plot=False):
     #if we don't want to plot
     else:
         for i in range(maxiter):
-            #update x0
+            #update x1
+            return A[0, :], x0
             x1 = np.array([x0[j] + (b[j] - np.inner(A[j, :], x0)) /A[j, j] for j in range(n)])
             #check convergence
             if la.norm(x0-x1, ord=np.inf) < tol:
                 break
+            #update previous
             x0 = x1
+            break
 
     return x1
 
@@ -154,16 +161,32 @@ def gauss_seidel_sparse(A, b, tol=1e-8, maxiter=100):
     Returns:
         x ((n,) ndarray): The solution to system Ax = b.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    #initialze the vectors
+    n = b.size
+    x0 = np.random.random(n)
+    #x0 = np.ones(n)
+    #compute iterations
+    for i in range(maxiter):
+        #iterate x1
+        x1 = np.array([x0[j] +
+                       (b[j] - np.inner(A.data[A.indptr[j]:A.indptr[j+1]],
+                                        x0[A.indices[A.indptr[j]:A.indptr[j+1]]]))
+                       / A[j, j] for j in range(n)])
+        #check convergence
+        if la.norm(x0-x1, ord=np.inf) < tol:
+            break
 
+        #update previous
+        x0 = x1
+
+    return x1
 
 # Problem 5
 def sor(A, b, omega, tol=1e-8, maxiter=100):
     """Calculate the solution to the system Ax = b via Successive Over-
     Relaxation.
 
-    Parameters:
-        A ((n, n) csr_matrix): A (n, n) sparse matrix.
+    Parameters: A ((n, n) csr_matrix): A (n, n) sparse matrix.
         b ((n, ) Numpy Array): A vector of length n.
         omega (float in [0,1]): The relaxation factor.
         tol (float): The convergence tolerance.
@@ -174,7 +197,29 @@ def sor(A, b, omega, tol=1e-8, maxiter=100):
         (bool): Whether or not Newton's method converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    #TODO: FInd out if omega is between 0 and 1 or greater than or equal to 1
+    #initialze the vectors and boolean
+    converged = False
+    n = b.size
+    x0 = np.random.random(n)
+    #x0 = np.ones(n)
+    #compute iterations
+    for i in range(maxiter):
+        #iterate x1
+        x1 = np.array([x0[j] +
+                       (b[j] - np.inner(A.data[A.indptr[j]:A.indptr[j+1]],
+                                        x0[A.indices[A.indptr[j]:A.indptr[j+1]]]))
+                       * omega / A[j, j] for j in range(n)])
+        #check convergence
+        if la.norm(x0-x1, ord=np.inf) < tol:
+            #update convergance boolean
+            converged = True
+            break
+
+        #update previous
+        x0 = x1
+
+    return x1, converged, i+1
 
 
 # Problem 6
@@ -195,7 +240,52 @@ def hot_plate(n, omega, tol=1e-8, maxiter=100, plot=False):
         (bool): Whether or not Newton's method converged.
         (int): The number of computed iterations in SOR.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    #subroutine to create the A matrix
+    def create_A(m):
+        #set up a sparse little matrix
+        B = sparse.lil_matrix((m, m))
+        #set the diagnonal elements to 4
+        B.setdiag(-4)
+        #set up the 1s and -1s next to the diagonal
+        B.setdiag(1, -1)
+        B.setdiag(1, 1)
+        #set up a sparse block diagonal matrix with B
+        A = sparse.block_diag([B] * m)
+        #set up identity matrix around B
+        A.setdiag([1] * m**2, m)
+        A.setdiag([1] * m**2, -m)
+        return A.tocsr()
+
+    #subroutine to create the u vector
+    def create_b(m):
+        #notice this pattern repeats
+        a = np.zeros(m)
+        a[0] = -100
+        a[-1] = -100
+
+        return np.tile(a, m)
+
+    A = create_A(n)
+    b = create_b(n)
+
+    u, convergence, iters = sor(A, b, omega, tol=tol, maxiter=maxiter)
+
+
+    if plot:
+        U = u.reshape((n, n))
+        #FIXME: Ask about creating grid
+        upper = np.max(u)
+        lower = np.min(u)
+        x = np.linspace(0, 10, n)
+        y = x.copy()
+        X, Y = np.meshgrid(x, y)
+
+        plt.pcolormesh(X, Y, U, cmap='coolwarm')
+        plt.show()
+
+
+    return u, convergence, iters
+
 
 
 # Problem 7
@@ -204,7 +294,27 @@ def prob7():
     and maxiter = 1000 with A and b generated with n=20. Plot the iterations
     computed as a function of omega.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    #FIXME: Omega does some weird stuff
+    omega = np.linspace(1, 1.95, 20)
+    iteration_count = []
+    converged = []
+
+    for w in omega:
+        _, c, i = hot_plate(20, w, tol=1e-2, maxiter=100, plot=False)
+        iteration_count.append(i)
+        converged.append(c)
+
+
+    plt.plot(omega, iteration_count, 'r:')
+    plt.title(r'SOR Iterations vs. $\omega$')
+    plt.xlabel(r'$\omega$')
+    plt.ylabel('Iteration Number')
+    plt.show()
+
+    min_index = np.argmin(iteration_count)
+
+    return omega[min_index]
+
 
 
 
@@ -231,3 +341,26 @@ if __name__ == "__main__":
     print(np.allclose(A@x3, b))
     print(np.allclose(x2, x3))
     '''
+
+    #prob4
+    '''
+    b = np.random.random(15000)
+    A = diag_dom(15000)
+    A = sparse.csr_matrix(A)
+
+    x4 = gauss_seidel_sparse(A, b, tol=1e-12, maxiter=150)
+    print(iters)
+    print(np.allclose(A @ x4, b))
+    '''
+
+    #prob5 can be tested like prob4
+
+    #prob6
+    '''
+    u, c, i = hot_plate(50, 0.5, tol=1e-8, maxiter=1000, plot=True)
+    print("converged:", c)
+    print("iterations:", i)
+    '''
+
+    #prob7
+    print(prob7())
